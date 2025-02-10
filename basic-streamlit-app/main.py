ca762f104d744fc1eb643efe49ee046a01d1e35f
Cameron Oglesby <coglesb2@nd.edu>
2025-02-09 22:21:47 -0500

    Update main.py

--- basic-streamlit-app/main.py
+++ basic-streamlit-app/main.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pandas as pd
 
+#Display a title:
+st.title()
+
+#Short description of what the app does:
+
+
+#Sample DataFrame:
+st.write("Here's a simple table about penguins:")
+df = pd.read_csv("data\penguins.csv")
+st.dataframe(df)
+
+#Interactive filtering options:
