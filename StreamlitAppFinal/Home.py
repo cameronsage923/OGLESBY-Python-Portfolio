caec0ff368f5c0019ad8cc91fcb7dbd661023634
Cameron Oglesby <coglesb2@nd.edu>
2025-05-08 17:40:13 -0400

    Updating REPE App

--- StreamlitAppFinal/Home.py
+++ StreamlitAppFinal/Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Multifamily RE Deal Tool", layout="centered")
 
 # Header Image
-st.image("pictures\Fundamentals-of-Value-Add-Real-Estate-Investing-01.png", use_column_width=True, caption="Source: CRE Knowledge Base")
+st.image("StreamlitAppFinal/pictures/Fundamentals-of-Value-Add-Real-Estate-Investing-01.png", use_column_width=True, caption="Source: CRE Knowledge Base")
 
 # Title Section:
 st.title("🏢 Real Estate Private Equity: Multifamily Value-Add Deal Modeling Lab")
@@ -24,7 +24,7 @@ st.markdown("""
 """)
 
 # --- Optional Image or Diagram ---
-st.image("pictures\sample_waterfall_distribution.png", caption="Sample Waterfall Structure", use_column_width=True)
+st.image("pictures/sample_waterfall_distribution.png", caption="Sample Waterfall Structure", use_column_width=True)
 
 # --- Footer or Credits ---
 st.markdown("---")
