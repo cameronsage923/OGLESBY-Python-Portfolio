1171594b368dfe1ae2dccc9adeb134e73f6701c8
Cameron Oglesby <coglesb2@nd.edu>
2025-05-08 17:56:51 -0400

    Update Home.py

--- StreamlitAppFinal/Home.py
+++ StreamlitAppFinal/Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Multifamily RE Deal Tool", layout="centered")
 
 # Header Image
-st.image("pictures/RE_image.png")
+#st.image("pictures/RE_image.png")
 
 # Title Section:
 st.title("🏢 Real Estate Private Equity: Multifamily Value-Add Deal Modeling Lab")
